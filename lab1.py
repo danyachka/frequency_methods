982ae8a8fe546da4f6926a187567340c5a02a125
DanilaKisikov <kisikov03@mail.ru>
2024-03-10 17:23:29 +0300

    Finish lab #1

--- lab1.py
+++ lab1.py
@@ -9,6 +9,14 @@ t0 = 0
 t1 = 1
 t2 = 2
 
+t0_c = 0
+t1_c = 8
+t2_c = 13
+t3_c = 21
+t4_c = 26
+
+NList = [1, 3, 4, 5, 15]
+
 
 class Plot:
 
@@ -45,10 +53,34 @@ def f_r(t):
     return np.sin((t * np.pi) - 0.8) - np.cos(4*t*np.pi)
 
 
-def draw(plots, title):
+def f_complex(space):
+    T = t4_c - t0_c
+    result = np.zeros(len(space), dtype=complex)
+
+    for i in range(len(space)):
+        t = space[i] % T
+
+        if t < t1_c:
+            result[i] = t + 1j * (8 - t)
+        elif t < t2_c:
+            t -= t1_c
+            result[i] = (8 - t) + 1j * (-t)
+        elif t < t3_c:
+            t -= t2_c
+            result[i] = (3 - t) + 1j * (0.375 * t - 5)
+        elif t <= t4_c:
+            t -= t3_c
+            result[i] = (t - 5) + 1j * (2 * t - 2)
+    return result
+
+
+def draw(plots, title, setLimits=True, isPeriodic=True):
+    if len(plots) == 0:
+        raise "Zero plots 've been given"
     ax = plt.subplot(1, 1, 1)
     colors = ["r", "b", "g", "p"]
-    labels = []
+    labelsY = []
+    labelsX = []
 
     T = t2 - t0
 
@@ -57,23 +89,26 @@ def draw(plots, title):
         x, y, xLabel, yLabel = plot.x, plot.y, plot.xLabel, plot.yLabel
 
         ax.plot(x, y, "-")
-        ax.plot(x - T, y, "-")
-        ax.plot(x + T, y, "-")
+        if isPeriodic:
+            ax.plot(x - T, y, "-")
+            ax.plot(x + T, y, "-")
 
-        labels.append(yLabel)
+        labelsY.append(yLabel)
+        labelsX.append(xLabel)
 
-    plt.xlabel("t")
-    plt.ylabel("f(t)")
-    plt.xlim(-2.5, 2.5)
-    plt.ylim(-1.2, 1.2)
+    plt.xlabel(labelsX[0])
+    plt.ylabel(labelsY[0])
+    if setLimits:
+        plt.xlim(-2.5, 2.5)
+        plt.ylim(-1.2, 1.2)
     plt.title(title)
     plt.grid(True)
-    plt.legend(labels)
+    plt.legend(labelsY)
     plt.show()
 
 
-def omega(n):
-    return 2*np.pi*n/(t2 - t0)
+def omega(n, T=t2-t0):
+    return 2*np.pi*n/T
 
 
 def calc(t, y, n):
@@ -92,7 +127,7 @@ def F_G_N(t, y, label, isSquare=False):
 
     T = t2 - t0
 
-    for N in [1, 3, 4, 5, 100]:
+    for N in NList:
         F = (2 / (t2 - t0)) * np.trapz(y * np.cos(omega(0) * t), t)
         G = 0
 
@@ -116,23 +151,23 @@ def F_G_N(t, y, label, isSquare=False):
     ##draw(plots, label)
 
 
-def perservalEquality(real, N):
+def perservalEquality(f, N):
     T = 2*np.pi
     t = np.linspace(-np.pi, np.pi, 1000)
 
-    norm_squared = np.trapz(np.abs(real(t)) ** 2, t)
+    norm_squared = np.trapz(np.abs(f(t)) ** 2, t)
 
     def an(n):
-        return np.trapz(real(t) * np.cos(2 * np.pi * n * t / T), t) * 2 / T
+        return np.trapz(f(t) * np.cos(2 * np.pi * n * t / T), t) * 2 / T
 
     def bn(n):
-        return np.trapz(real(t) * np.sin(2 * np.pi * n * t / T), t) * 2 / T
+        return np.trapz(f(t) * np.sin(2 * np.pi * n * t / T), t) * 2 / T
 
     aList = [an(i) for i in range(N + 1)]
     bList = [bn(i) for i in range(1, N + 1)]
 
     def cn(n):
-        return np.trapz(real(t) * np.exp(-1j * 2 * np.pi * n * t / T), t) / T
+        return np.trapz(f(t) * np.exp(-1j * 2 * np.pi * n * t / T), t) / T
 
     c = [cn(i) for i in range(N + 1)]
 
@@ -149,7 +184,7 @@ def createDefaultPlot():
     y = f_sq(t)
     label = "Квадратная функция"
     perservalEquality(f_sq, 100)
-    F_G_N(t, y, label)
+    ##F_G_N(t, y, label)
 
     y = f_c(t)
     label = "Чётная функция"
@@ -157,12 +192,39 @@ def createDefaultPlot():
 
     y = f_nc(t)
     label = "Нечётная функция"
-    ##F_G_N(t, y, label)
+    F_G_N(t, y, label)
 
     y = f_r(t)
     label = "Случайная функция"
     ##F_G_N(t, y, label)
 
 
+def createComplex():
+    t = np.linspace(t0_c, t4_c, 10000)
+
+    c = f_complex(t)
+    label = "Комплексная функция"
+
+    for N in NList:
+        G = 0
+        for n in range(-N - 1, N + 1):
+            omg = omega(n, t4_c - t0_c)
+            cn = (1 / t4_c) * np.trapz(c * np.exp(-1j * omg * t), t)
+            if n == N:
+                print(cn)
+
+            G += cn * (np.exp(1j * omg * t))
+
+        G_r = np.real(G)
+        G_i = np.imag(G)
+        draw([Plot(t, G_i, "t", "G_i_n(t) n = " + str(N))], label + " G_i(t)", setLimits=False, isPeriodic=False)
+        draw([Plot(t, G_r, "t", "G_r_n(t) n = " + str(N))], label + " G_r(t)", setLimits=False, isPeriodic=False)
+        draw([Plot(G_r, G_i, "t", "Gn(t) n = " + str(N))],
+             label, setLimits=False, isPeriodic=False)
+
+    draw([Plot(np.real(c), np.imag(c), "", "")],
+         label, setLimits=False, isPeriodic=False)
+
+
 if __name__ == '__main__':
-    createDefaultPlot()
+    createComplex()
